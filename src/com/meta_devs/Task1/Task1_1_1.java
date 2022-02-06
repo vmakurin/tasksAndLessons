06bc61dc1d60b030844a8c83615e0b0fe87f2843
vmakurin <vmakurin@meta-devs.io>
2022-02-06 23:04:21 +0300

    Add Task1_3

---
+++ src/com/meta_devs/Task1/Task1_1_1.java
@@ -0,0 +1,16 @@
+package com.meta_devs.Task1;
+
+import java.util.Scanner;
+
+public class Task1_1_1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
